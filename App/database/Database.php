b082b09f199f754c6d1c08734eda0dbf726e62e8
MD12nice <lucassouza0807@gmail.com>
2021-09-18 19:17:35 -0300

    Database issues fixed

--- App/database/Database.php
+++ App/database/Database.php
@@ -2,6 +2,10 @@
 
 namespace App\Database ;
 
+require_once __DIR__."/../../vendor/autoload.php" ;
+
+use PDO ;
+
 class DB 
 {
     protected $database ;
@@ -10,7 +14,7 @@ class DB
     {
         try{
 
-            include_once __DIR__."/../../database/database.conf.php" ;
+            require_once __DIR__."/config.php" ;
             
             $db =  new PDO("`{DRIVER}:host={HOST}; dbname={DB_HOST}", "DB_USER", "{DB_PASSWORD}");
             $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
--- App/database/config.php
+++ App/database/config.php
@@ -1,8 +1,8 @@
 <?php 
 
-require_once __DIR__."/../vendor/autoload.php" ;
+require_once __DIR__."/../../vendor/autoload.php" ;
 
-$env = Dotenv\Dotenv::createImmutable(__DIR__."/..");
+$env = Dotenv\Dotenv::createImmutable(__DIR__."/../..");
 $env->load();
 
 define("DRIVER", $_ENV['DRIVER']);
@@ -10,5 +10,3 @@ define("HOST", $_ENV['HOST']);
 define("DB_NAME", $_ENV['DB_NAME']);
 define("DB_USER", $_ENV['DB_USER']);
 define("DB_PASSWORD", $_ENV["DB_PASSWORD"]);
-
-echo  DB_PASSWORD ;
